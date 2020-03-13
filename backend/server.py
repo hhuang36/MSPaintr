d9e617bf17bd4b27e909bdedd5e903f9484a26b3
primemini <vjvitale@buffalo.edu>
2020-03-13 03:52:56 -0400

    finished base server

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 #Source: Bottle
 
 
-from bottle import get, post, request, run, response, view
+from bottle import get, post, request, route,redirect, run, response
 
 
 def readFile(filename):
@@ -12,21 +12,95 @@ def readFile(filename):
 			result += line + '\r\n'
 	return result 
 
+def createImageFile(filename):
+	body = ""
+	with open(filename, "rb") as f:
+		body = f.read()
+
+	return body
+
 
 @get('/')
-@view('template')
 def serve_home():
-	return dict(src="/index")
+	return readFile("../frontend/src/components/Home.html")
 
-@get('/App')
+@route('/home')
 def getFeed():
-	response.content_type="text/javascript"
-	return readFile("../frontend/src/App.js")
+	redirect('/')
 
-@get('/index')
-def getIndex():
-	response.content_type="text/javascript"
-	return readFile("../frontend/src/index.js")
+@get("/App.css")
+def serveAppCSS():
+	response.content_type = 'text/css; charset=utf-8'
+	return readFile("../frontend/src/App.css")
+
+@get('/login')
+def serveLogin():
+	return readFile("../frontend/src/components/login/Login.html")
+
+@get("/Login.css")
+def serveLoginCSS():
+	response.content_type = 'text/css; charset=utf-8'
+	return readFile("../frontend/src/components/login/Login.css")
+
+@get("/register")
+def serveRegister():
+	return readFile("../frontend/src/components/register/Regristration.html")
+
+@get("/Regristration.css")
+def serveRegisterCSS():
+	response.content_type = 'text/css; charset=utf-8'
+	return readFile("../frontend/src/components/register/Regristration.css")
+
+@get("/profile")
+def serveProfile():
+	return readFile("../frontend/src/components/profile/Profile.html")
+
+@get("/Profile.css")
+def serveProfileCSS():
+	response.content_type = 'text/css; charset=utf-8'
+	return readFile("../frontend/src/components/profile/Profile.css")
+
+@get("/directmessages")
+def serveDMS():
+	return readFile("../frontend/src/components/dms/DirectMessages.html")
+
+@get("/DirectMessages.css")
+def serveDMSCSS():
+	response.content_type = 'text/css; charset=utf-8'
+	return readFile("../frontend/src/components/dms/DirectMessages.css")
+
+@get("/newpost")
+def serveNewPost():
+	return readFile("../frontend/src/components/NewPost.html")
+
+@get("/seemore")
+def serveMore():
+	return readFile("../frontend/src/components/SeeMore.html")
+
+@get("/MSPaintRLogo.png")
+def serveLogo():
+	response.content_type = "image/png; charset=ascii"
+	return createImageFile("../frontend/src/components/profile/profileimages/MSPaintRLogo.png")
+
+@get("/testimage.png")
+def serveLogo():
+	response.content_type = "image/png; charset=ascii"
+	return createImageFile("../frontend/src/components/testimages/testimage.png")
+
+@get("/testimage2.png")
+def serveLogo():
+	response.content_type = "image/png; charset=ascii"
+	return createImageFile("../frontend/src/components/testimages/testimage2.png")
+
+@get("/eggie.png")
+def serveLogo():
+	response.content_type = "image/png; charset=ascii"
+	return createImageFile("../frontend/src/components/profile/profileimages/eggie.png")
+
+@get("/subtle_lgbt.png")
+def serveLogo():
+	response.content_type = "image/png; charset=ascii"
+	return createImageFile("../frontend/src/components/profile/profileimages/subtle_lgbt.png")
 
 if __name__ == "__main__":
-	run(host='localhost', port=8080)
+	run(host='localhost', port=8000)

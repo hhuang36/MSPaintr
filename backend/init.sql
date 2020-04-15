6e6842c26ce6d9076707570dd65fc83183433bdb
edentron <cleverforesttrees@gmail.com>
2020-04-15 15:33:55 -0400

    Updating Docker-Compose and Created Initial Config of Database #28 #31
    init.sql in backend should initialize the database. The docker-compose file successfully runs our frontend app. Unsure how to test if database exists.

---
+++ backend/init.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS mspaintrdb
+USE mspaintrdb;
+CREATE TABLE IF NOT EXISTS Users(
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(45),
+    bio VARCHAR(255),
+    followers INT
+    );
+CREATE TABLE IF NOT EXISTS Posts (
+    postid VARCHAR(255),
+    image VARCHAR(255),
+    upvotes INT,
+    Users_username VARCHAR (20)
+    );
+CREATE TABLE IF NOT EXISTS Comments(
+    commentid INT,
+    comment VARCHAR(255),
+    Posts_postid VARCHAR(255),
+    Users_username VARCHAR(20)
+    );
+
+INSERT INTO Users (username, password, bio, followers)
+VALUES ('defaultuser', 'defaultpass', 'This is the default bio!', 0);
